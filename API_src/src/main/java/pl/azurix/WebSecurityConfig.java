a26dde0b760d1c75fa69be3c28cec927194bc5be
nerio2 <Patryknerio2@gmail.com>
2019-01-07 21:32:50 +0100

    testing...

--- API_src/src/main/java/pl/azurix/WebSecurityConfig.java
+++ API_src/src/main/java/pl/azurix/WebSecurityConfig.java
@@ -38,7 +38,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+        http.csrf().disable().authorizeRequests()
+                .antMatchers("/oauth/token").permitAll()
                 .antMatchers("/auth/*").permitAll()
                 .antMatchers("/root/*").hasRole("ROOT")
                 .anyRequest().hasRole("USER")
