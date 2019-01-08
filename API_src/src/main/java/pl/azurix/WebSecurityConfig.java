d81fa9d02bbed0edf14b5f88b0153f66879db1fe
nerio2 <Patryknerio2@gmail.com>
2019-01-08 22:12:49 +0100

    testingv2.4

--- API_src/src/main/java/pl/azurix/WebSecurityConfig.java
+++ API_src/src/main/java/pl/azurix/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package pl.azurix;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -38,8 +39,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors().and().csrf().disable().authorizeRequests()
-                .antMatchers("/oauth/token").permitAll()
+        http.csrf().disable().authorizeRequests()
+                .antMatchers(HttpMethod.OPTIONS,"/oauth/token").permitAll()
                 .antMatchers("/auth/*").permitAll()
                 .antMatchers("/root/*").hasRole("ROOT")
                 .anyRequest().hasRole("USER")
