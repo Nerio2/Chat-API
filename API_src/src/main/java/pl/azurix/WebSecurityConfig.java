b9cbb748fa62b2c46eb7b0a23c2e3774aaa04190
nerio2 <Patryknerio2@gmail.com>
2019-01-09 18:24:10 +0100

    testingv3.4

--- API_src/src/main/java/pl/azurix/WebSecurityConfig.java
+++ API_src/src/main/java/pl/azurix/WebSecurityConfig.java
@@ -45,7 +45,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
        // http.addFilterBefore(new SimpleCorsFilter(), BasicAuthenticationFilter.class);
-        http.cors().and().authorizeRequests()
+        http.authorizeRequests()
                 .antMatchers("/oauth2/*").permitAll()
                 .antMatchers(HttpMethod.OPTIONS,"/oauth/token").permitAll()
                 .antMatchers("/auth/*").permitAll()
